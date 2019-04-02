5c161ee4bc3b44a331a51c1624d969eb49daf693
CodeNotAcceptable <36109227+code406@users.noreply.github.com>
2019-04-02 22:53:13 +0200

    Changed tabs to spaces so indentation won't go mad

--- pract3a.asm
+++ pract3a.asm
@@ -1,163 +1,163 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; 		        	  SBM 2016. Practica 3 - EJERCICIO 3a				       ;
-;           		  Pareja	10 - Ana Roa, David Palomo				       ;
+;                       SBM 2016. Practica 3 - EJERCICIO 3a                    ;
+;                     Pareja    10 - Ana Roa, David Palomo                     ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-_TEXT SEGMENT BYTE PUBLIC 'CODE' 	;; Definicion del segmento de codigo
+_TEXT SEGMENT BYTE PUBLIC 'CODE' ;; Definicion del segmento de codigo
 ASSUME CS:_TEXT
 
 ;------------------------------------------------------------------------------;
 ;            unsigned char computeControlDigit(char* barCodeASCII)             ;
 ;------------------------------------------------------------------------------;
-PUBLIC _computeControlDigit			;; Hacer visible/accesible desde C
+PUBLIC _computeControlDigit      ;; Hacer visible/accesible desde C
 _computeControlDigit PROC FAR
 
-	PUSH BP 					    ;; Salvaguardar BP en la pila
-	MOV BP, SP						;; Igualar BP el contenido de SP
-	PUSH BX CX SI DS				;; Salvaguardar registros en pila
-	LDS BX, [BP+6]					;; DS=Segmento  BX=Offset (barCodeASCII)
+    PUSH BP                      ;; Salvaguardar BP en la pila
+    MOV BP, SP                   ;; Igualar BP el contenido de SP
+    PUSH BX CX SI DS             ;; Salvaguardar registros en pila
+    LDS BX, [BP+6]               ;; DS=Segmento  BX=Offset (barCodeASCII)
 
-	XOR CX, CX						;; Inicializar las dos sumas a 0
-	XOR SI, SI						;; i = 0
-	BUCLEIMPAR:
-		ADD CL, DS:[BX][SI] 		;; CL = Suma Impares
-		SUB CL, 48 					;; ASCII a decimal
-		ADD SI, 2
-		CMP SI, 12
-		JNE BUCLEIMPAR
+    XOR CX, CX                   ;; Inicializar las dos sumas a 0
+    XOR SI, SI                   ;; i = 0
+    BUCLEIMPAR:
+        ADD CL, DS:[BX][SI]      ;; CL = Suma Impares
+        SUB CL, 48               ;; ASCII a decimal
+        ADD SI, 2
+        CMP SI, 12
+        JNE BUCLEIMPAR
 
-	MOV SI, 1						;; i = 1
-	BUCLEPAR:
-		ADD CH, DS:[BX][SI]			;; CH = Suma Pares
-		SUB CH, 48 					;; ASCII a decimal
-		ADD SI, 2
-		CMP SI, 13
-		JNE BUCLEPAR
+    MOV SI, 1                    ;; i = 1
+    BUCLEPAR:
+        ADD CH, DS:[BX][SI]      ;; CH = Suma Pares
+        SUB CH, 48               ;; ASCII a decimal
+        ADD SI, 2
+        CMP SI, 13
+        JNE BUCLEPAR
 
-	MOV AL, 3
-	MUL CH							;; AL = (Suma Pares) * 3
-	ADD AL, CL						;; AL = (Suma Pares) * 3 + (Suma Impares)
-	MOV DL, AL						;; Guardar AL en DL porque se pierde en DIV
+    MOV AL, 3
+    MUL CH                       ;; AL = (Suma Pares) * 3
+    ADD AL, CL                   ;; AL = (Suma Pares) * 3 + (Suma Impares)
+    MOV DL, AL                   ;; Guardar AL en DL porque se pierde en DIV
 
-	MOV BL, 10						;; Decena Superior de n = n + (10 - n % 10)
-	DIV BL 							;; AL = AL / 10     AH: Resto
-	MOV AL, 10
-	SUB AL, AH  					;; AL = 10 - Resto
-	ADD AL, DL						;; AL = Decena Superior
+    MOV BL, 10                   ;; Decena Superior de n = n + (10 - n % 10)
+    DIV BL                       ;; AL = AL / 10     AH: Resto
+    MOV AL, 10
+    SUB AL, AH                   ;; AL = 10 - Resto
+    ADD AL, DL                   ;; AL = Decena Superior
 
-	XOR AH, AH						;; Limpiar AH para devolver AX
-	SUB AL, DL  					;; AL = Decena Superior - DL
-	CMP AL, 10						;; Comprobar caso de que digito salga 10
-	JNE TERMINA						;; Si AL != 10, termina
-	XOR AL, AL						;; Si AL == 10, AL = 0
+    XOR AH, AH                   ;; Limpiar AH para devolver AX
+    SUB AL, DL                   ;; AL = Decena Superior - DL
+    CMP AL, 10                   ;; Comprobar caso de que digito salga 10
+    JNE TERMINA                  ;; Si AL != 10, termina
+    XOR AL, AL                   ;; Si AL == 10, AL = 0
 
-TERMINA:							;; AX = Resultado Digito de Control
-	POP DS SI CX BX BP				;; Restaurar registros antes de salir
-	RET								;; Retorno de funcion. Devuelve digito en AX
+TERMINA:                         ;; AX = Resultado Digito de Control
+    POP DS SI CX BX BP           ;; Restaurar registros antes de salir
+    RET                          ;; Retorno de funcion. Devuelve digito en AX
 
-_computeControlDigit ENDP			;; Termina la funcion computeControlDigit
+_computeControlDigit ENDP        ;; Termina la funcion computeControlDigit
 
 
 
 ;------------------------------------------------------------------------------;
-;  void decodeBarCode(unsigned char* in_barCodeASCII,						   ;
-;					  unsigned int* countryCode,  unsigned int* companyCode,   ;
-;					  unsigned long* productCode, unsigned char* controlDigit) ;
+;  void decodeBarCode(unsigned char* in_barCodeASCII,                          ;
+;                     unsigned int* countryCode,  unsigned int* companyCode,   ;
+;                     unsigned long* productCode, unsigned char* controlDigit) ;
 ;------------------------------------------------------------------------------;
-PUBLIC _decodeBarCode				;; Hacer visible/accesible desde C
+PUBLIC _decodeBarCode            ;; Hacer visible/accesible desde C
 _decodeBarCode PROC FAR
 
-	PUSH BP 					    ;; Salvaguardar BP en la pila
-	MOV BP, SP						;; Igualar BP el contenido de SP
-	PUSH AX BX CX DX SI DI DS		;; Salvaguardar registros en pila
+    PUSH BP                      ;; Salvaguardar BP en la pila
+    MOV BP, SP                   ;; Igualar BP el contenido de SP
+    PUSH AX BX CX DX SI DI DS    ;; Salvaguardar registros en pila
 
-	; COUNTRY = ds:[bx][0,1,2]
-	LDS BX, [BP+6]					;; DS=Segmento  BX=Offset (in_BarCodeASCII)
-	MOV SI, 3
-	MOV AX, 1
-	XOR DI, DI
-	XOR DH, DH
+    ; COUNTRY = ds:[bx][0,1,2]
+    LDS BX, [BP+6]               ;; DS=Segmento  BX=Offset (in_BarCodeASCII)
+    MOV SI, 3
+    MOV AX, 1
+    XOR DI, DI
+    XOR DH, DH
 COUNTRYROAD:
-	MOV CX, AX						;; Guardar AX para no perderlo en el MUL
-	MOV DL, DS:[BX][SI-1]			;; Leer argumento de derecha a izquierda
-	SUB DL, 48						;; ASCII a decimal
-	MUL DX							;; AX = ValorLeido * AX
-	ADD DI, AX						;; DI = Valor decimal final
-	MOV AX, CX						;; Recuperar AX
-	MOV CX, 10
-	MUL CX 							;; AX = 1.. 10.. 100..
-	DEC SI
-	JNZ COUNTRYROAD
+    MOV CX, AX                   ;; Guardar AX para no perderlo en el MUL
+    MOV DL, DS:[BX][SI-1]        ;; Leer argumento de derecha a izquierda
+    SUB DL, 48                   ;; ASCII a decimal
+    MUL DX                       ;; AX = ValorLeido * AX
+    ADD DI, AX                   ;; DI = Valor decimal final
+    MOV AX, CX                   ;; Recuperar AX
+    MOV CX, 10
+    MUL CX                       ;; AX = 1.. 10.. 100..
+    DEC SI
+    JNZ COUNTRYROAD
 
-	LDS BX, [BP+10]					;; Cargar DS para escribir el CountryCode
-	MOV WORD PTR DS:[BX], DI		;; Escribir valor decimal
+    LDS BX, [BP+10]              ;; Cargar DS para escribir el CountryCode
+    MOV WORD PTR DS:[BX], DI     ;; Escribir valor decimal
 
-	; COMPANY = ds:[bx][3,4,5,6]
-	LDS BX, [BP+6]					;; DS=Segmento  BX=Offset (in_BarCodeASCII)
-	MOV SI, 6
-	MOV AX, 1
-	XOR DI, DI
+    ; COMPANY = ds:[bx][3,4,5,6]
+    LDS BX, [BP+6]               ;; DS=Segmento  BX=Offset (in_BarCodeASCII)
+    MOV SI, 6
+    MOV AX, 1
+    XOR DI, DI
 COMPANYLOOP:
-	XOR DH, DH						;; Quitar lo que haya en DH (p.ej. del MUL)
-	MOV CX, AX						;; Guardar AX para no perderlo en el MUL
-	MOV DL, DS:[BX][SI]				;; Leer argumento de derecha a izquierda
-	SUB DL, 48						;; ASCII a decimal
-	MUL DX							;; AX = ValorLeido * AX
-	ADD DI, AX						;; DI = Valor decimal final
-	MOV AX, CX						;; Recuperar AX
-	MOV CX, 10
-	MUL CX 							;; AX = 1.. 10.. 100..
-	DEC SI
-	CMP SI, 2
-	JNE COMPANYLOOP
+    XOR DH, DH                   ;; Quitar lo que haya en DH (p.ej. del MUL)
+    MOV CX, AX                   ;; Guardar AX para no perderlo en el MUL
+    MOV DL, DS:[BX][SI]          ;; Leer argumento de derecha a izquierda
+    SUB DL, 48                   ;; ASCII a decimal
+    MUL DX                       ;; AX = ValorLeido * AX
+    ADD DI, AX                   ;; DI = Valor decimal final
+    MOV AX, CX                   ;; Recuperar AX
+    MOV CX, 10
+    MUL CX                       ;; AX = 1.. 10.. 100..
+    DEC SI
+    CMP SI, 2
+    JNE COMPANYLOOP
 
-	LDS BX, [BP+14]					;; Cargar DS para escribir el CompanyCode
-	MOV WORD PTR DS:[BX], DI		;; Escribir valor decimal
+    LDS BX, [BP+14]              ;; Cargar DS para escribir el CompanyCode
+    MOV WORD PTR DS:[BX], DI     ;; Escribir valor decimal
 
-	; PRODUCT = ds:[bx][7,8,9,10,11]
-	LDS BX, [BP+6]					;; DS=Segmento  BX=Offset (in_BarCodeASCII)
-	MOV SI, 11
-	MOV AX, 1
-	XOR DI, DI
+    ; PRODUCT = ds:[bx][7,8,9,10,11]
+    LDS BX, [BP+6]               ;; DS=Segmento  BX=Offset (in_BarCodeASCII)
+    MOV SI, 11
+    MOV AX, 1
+    XOR DI, DI
 PRODUCTLOOP:
-	XOR DH, DH						;; Quitar lo que haya en DH (p.ej. del MUL)
-	MOV CX, AX						;; Guardar AX para no perderlo en el MUL
-	MOV DL, DS:[BX][SI]				;; Leer argumento de derecha a izquierda
-	SUB DL, 48						;; ASCII a decimal
-	MUL DX							;; AX = ValorLeido * AX		 "No cabe" en AX
-	ADD DI, AX						;; DI = Valor decimal final	 "No cabe" en DI
-	JNC CHECK_MUL					;; Comprueba carry (suma de ultima vuelta)
-	INC DX							;; Si hay carry, suma > 65.535 (suma > FFFF)
-	JMP BREAK						;; DX = 1 para anadirlo delante del result
-CHECK_MUL:							;; Comprueba overflow (mul de ultima vuelta)
-	CMP DX, 1						;; Si DX=1 es que no cupo en AX (digito > 6)
-	JE BREAK						;; El 1 de DX se anadira delante del result
-	MOV AX, CX						;; Recuperar AX
-	MOV CX, 10
-	MUL CX 							;; AX = 1.. 10.. 100..
-	DEC SI
-	CMP SI, 6
-	JNE PRODUCTLOOP
+    XOR DH, DH                   ;; Quitar lo que haya en DH (p.ej. del MUL)
+    MOV CX, AX                   ;; Guardar AX para no perderlo en el MUL
+    MOV DL, DS:[BX][SI]          ;; Leer argumento de derecha a izquierda
+    SUB DL, 48                   ;; ASCII a decimal
+    MUL DX                       ;; AX = ValorLeido * AX       "No cabe" en AX
+    ADD DI, AX                   ;; DI = Valor decimal final   "No cabe" en DI
+    JNC CHECK_MUL                ;; Comprueba carry (suma de ultima vuelta)
+    INC DX                       ;; Si hay carry, suma > 65.535 (suma > FFFF)
+    JMP BREAK                    ;; DX = 1 para anadirlo delante del result
+CHECK_MUL:                       ;; Comprueba overflow (mul de ultima vuelta)
+    CMP DX, 1                    ;; Si DX=1 es que no cupo en AX (digito > 6)
+    JE BREAK                     ;; El 1 de DX se anadira delante del result
+    MOV AX, CX                   ;; Recuperar AX
+    MOV CX, 10
+    MUL CX                       ;; AX = 1.. 10.. 100..
+    DEC SI
+    CMP SI, 6
+    JNE PRODUCTLOOP
 
-	XOR DX, DX						;; Si no hubo desbordamiento, DX debe ser 0
+    XOR DX, DX                   ;; Si no hubo desbordamiento, DX debe ser 0
 BREAK:
-	LDS BX, [BP+18]					;; Cargar DS para escribir el ProductCode
-	MOV WORD PTR DS:[BX], DI		;; Escribir LSW del long
-	MOV WORD PTR DS:[BX+2], DX		;; Escribir MSW del long (DX, que es 0 o 1)
+    LDS BX, [BP+18]              ;; Cargar DS para escribir el ProductCode
+    MOV WORD PTR DS:[BX], DI     ;; Escribir LSW del long
+    MOV WORD PTR DS:[BX+2], DX   ;; Escribir MSW del long (DX, que es 0 o 1)
 
-	; CONTROL_DIGIT = ds:[bx][12]
-	LDS BX, [BP+6]					;; DS=Segmento  BX=Offset (in_BarCodeASCII)
-	MOV SI, 12						;; ControlDigit esta en la posicion 12
-	MOV DL, DS:[BX][SI]				;; Leer byte de controlDigit
-	SUB DL, 48						;; ASCII a decimal
-	XOR DH, DH
-	LDS BX, [BP+22]					;; Cargar DS para escribir el controlDigit
-	MOV WORD PTR DS:[BX], DX		;; Escribir valor decimal
+    ; CONTROL_DIGIT = ds:[bx][12]
+    LDS BX, [BP+6]               ;; DS=Segmento  BX=Offset (in_BarCodeASCII)
+    MOV SI, 12                   ;; ControlDigit esta en la posicion 12
+    MOV DL, DS:[BX][SI]          ;; Leer byte de controlDigit
+    SUB DL, 48                   ;; ASCII a decimal
+    XOR DH, DH
+    LDS BX, [BP+22]              ;; Cargar DS para escribir el controlDigit
+    MOV WORD PTR DS:[BX], DX     ;; Escribir valor decimal
 
-	POP DS DI SI DX CX BX AX BP		;; Restaurar registros antes de salir
-	RET								;; Retorno de funcion. Devuelve digito en AX
+    POP DS DI SI DX CX BX AX BP  ;; Restaurar registros antes de salir
+    RET                          ;; Retorno de funcion. Devuelve digito en AX
 
-_decodeBarCode ENDP					;; Termina la funcion decodeBarCode
+_decodeBarCode ENDP              ;; Termina la funcion decodeBarCode
 
 _TEXT ENDS
 END
--- pract3b.asm
+++ pract3b.asm
@@ -1,77 +1,77 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; 		        	  SBM 2016. Practica 3 - EJERCICIO 3b				       ;
-;           		  Pareja	10 - Ana Roa, David Palomo				       ;
+;                       SBM 2016. Practica 3 - EJERCICIO 3b                    ;
+;                     Pareja    10 - Ana Roa, David Palomo                     ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-_TEXT SEGMENT BYTE PUBLIC 'CODE' 	;; Definicion del segmento de codigo
+_TEXT SEGMENT BYTE PUBLIC 'CODE' ;; Definicion del segmento de codigo
 ASSUME CS:_TEXT
 
 ;------------------------------------------------------------------------------;
-;	void createBarCode(int countryCode,  unsigned int companyCode,			   ;
-;					   unsigned long productCode,  unsigned char controlDigit, ;
-;					   unsigned char* out_barCodeASCII);					   ;
+;    void createBarCode(int countryCode,  unsigned int companyCode,            ;
+;                       unsigned long productCode, unsigned char controlDigit, ;
+;                       unsigned char* out_barCodeASCII);                      ;
 ;------------------------------------------------------------------------------;
-PUBLIC _createBarCode				;; Hacer visible/accesible desde C
+PUBLIC _createBarCode            ;; Hacer visible/accesible desde C
 _createBarCode PROC FAR
 
-	PUSH BP 					    ;; Salvaguardar BP en la pila
-	MOV BP, SP						;; Igualar BP el contenido de SP
-	PUSH AX BX CX DX SI DI DS		;; Salvaguardar registros en pila
+    PUSH BP                      ;; Salvaguardar BP en la pila
+    MOV BP, SP                   ;; Igualar BP el contenido de SP
+    PUSH AX BX CX DX SI DI DS    ;; Salvaguardar registros en pila
 
-	LDS BX, [BP+16]					;; Cargar out_barCodeASCII para escribirlo
+    LDS BX, [BP+16]              ;; Cargar out_barCodeASCII para escribirlo
 
-	; ControlDigit
-	MOV SI, 12
-	MOV DI, 48
-	MOV DS:[BX][SI], DI				;; Vaciar caracter control (si no, error)
-	MOV AL, [BP+14]					;; AL = ControlDigit (argumento, decimal)
-	ADD AL, 48						;; Convertir ControlDigit a ASCII
-	MOV DS:[BX][SI], AL				;; Colocar en cadena[12]
+    ; ControlDigit
+    MOV SI, 12
+    MOV DI, 48
+    MOV DS:[BX][SI], DI          ;; Vaciar caracter control (si no, error)
+    MOV AL, [BP+14]              ;; AL = ControlDigit (argumento, decimal)
+    ADD AL, 48                   ;; Convertir ControlDigit a ASCII
+    MOV DS:[BX][SI], AL          ;; Colocar en cadena[12]
 
-	; ProductCode
-	MOV AX, [BP+10]					;; DX:AX = ProductCode
-	MOV DX, [BP+12] 				;; DX puede ser 0 o 1
-	DEC SI							;; Bucle: separa digitos y convierte a ASCII
-	JMP NO_LIMPIAR					;; Evita limpiar DX en la primera vuelta
-SEPARA_PROD:						;; ASCII y lo escribe en la cadena
-	XOR DX, DX						;; Limpiar DX porque DIV guarda resto en el
-	NO_LIMPIAR:
-	MOV CX, 10
-	DIV CX         					;; DX = Resto de dividir AX / 10
-	ADD DL, 48						;; Convierte el resto a ASCII
-	MOV DS:[BX][SI], DL  			;; Colocar resto en cadena[11,10,9,8,7]
-	DEC SI							;; i--
-	CMP SI, 6
-	JNE SEPARA_PROD
+    ; ProductCode
+    MOV AX, [BP+10]              ;; DX:AX = ProductCode
+    MOV DX, [BP+12]              ;; DX puede ser 0 o 1
+    DEC SI                       ;; Bucle: separa digitos y convierte a ASCII
+    JMP NO_LIMPIAR               ;; Evita limpiar DX en la primera vuelta
+SEPARA_PROD:                     ;; ASCII y lo escribe en la cadena
+    XOR DX, DX                   ;; Limpiar DX porque DIV guarda resto en el
+    NO_LIMPIAR:
+    MOV CX, 10
+    DIV CX                       ;; DX = Resto de dividir AX / 10
+    ADD DL, 48                   ;; Convierte el resto a ASCII
+    MOV DS:[BX][SI], DL          ;; Colocar resto en cadena[11,10,9,8,7]
+    DEC SI                       ;; i--
+    CMP SI, 6
+    JNE SEPARA_PROD
 
-	; CountryCode
-	MOV AX, [BP+8]					;; AX = countryCode
-SEPARA_COUNTRY:						;; Bucle: separa digitos y convierte a ASCII
-	XOR DX, DX						;; Limpiar DX porque DIV guarda resto en el
-	MOV CX, 10
-	DIV CX         					;; DX = Resto de dividir AX / 10
-	ADD DL, 48						;; Convierte el resto a ASCII
-	MOV DS:[BX][SI], DL  			;; Colocar resto en cadena[6,5,4,3]
-	DEC SI							;; i--
-	CMP SI, 2
-	JNE SEPARA_COUNTRY
+    ; CountryCode
+    MOV AX, [BP+8]               ;; AX = countryCode
+SEPARA_COUNTRY:                  ;; Bucle: separa digitos y convierte a ASCII
+    XOR DX, DX                   ;; Limpiar DX porque DIV guarda resto en el
+    MOV CX, 10
+    DIV CX                       ;; DX = Resto de dividir AX / 10
+    ADD DL, 48                   ;; Convierte el resto a ASCII
+    MOV DS:[BX][SI], DL          ;; Colocar resto en cadena[6,5,4,3]
+    DEC SI                       ;; i--
+    CMP SI, 2
+    JNE SEPARA_COUNTRY
 
-	; CompanyCode
-	MOV AX, [BP+6]					;; AX = companyCode
-SEPARA_COMP:						;; Bucle: separa digitos y convierte a ASCII
-	XOR DX, DX						;; Limpiar DX porque DIV guarda resto en el
-	MOV CX, 10
-	DIV CX         					;; DX = Resto de dividir AX / 10
-	ADD DL, 48						;; Convierte el resto a ASCII
-	MOV DS:[BX][SI], DL  			;; Colocar resto en cadena[2,1,0]
-	DEC SI							;; i--
-	CMP SI, -1
-	JNE SEPARA_COMP
+    ; CompanyCode
+    MOV AX, [BP+6]               ;; AX = companyCode
+SEPARA_COMP:                     ;; Bucle: separa digitos y convierte a ASCII
+    XOR DX, DX                   ;; Limpiar DX porque DIV guarda resto en el
+    MOV CX, 10
+    DIV CX                       ;; DX = Resto de dividir AX / 10
+    ADD DL, 48                   ;; Convierte el resto a ASCII
+    MOV DS:[BX][SI], DL          ;; Colocar resto en cadena[2,1,0]
+    DEC SI                       ;; i--
+    CMP SI, -1
+    JNE SEPARA_COMP
 
-	POP DS DI SI DX CX BX AX BP		;; Restaurar registros antes de salir
-	RET								;; Retorno de funcion. Devuelve digito en AX
+    POP DS DI SI DX CX BX AX BP  ;; Restaurar registros antes de salir
+    RET                          ;; Retorno de funcion. Devuelve digito en AX
 
-_createBarCode ENDP					;; Termina la funcion computeControlDigit
+_createBarCode ENDP              ;; Termina la funcion computeControlDigit
 
 _TEXT ENDS
 END
